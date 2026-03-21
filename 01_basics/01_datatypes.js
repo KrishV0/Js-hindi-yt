d2a53fda3e01c922bea6eed7b9f2fa2086bc9a80
KrishV0 <krishsharma2606@gmail.com>
2026-03-21 08:14:41 +0000

    datatypes and variables

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat the entire js code  as a newer version
+//alert(2+2)  works on browser not on an IDE(Nodejs)
+
+let name = "Krish"
+let rank = "Sixpath"
+let age = 19
+let loggedIn = false
+let state;
+
+// Number range
+/*Maximum safe value:
+2^53 - 1 = 9007199254740991
+
+Minimum safe value:
+-(2^53 - 1) = -9007199254740991*/
+
+//bigint 
+//String =""
+// boolean - true or false
+//null - standalone value (OBJECT)
+//undefined - value hasnt been assigned yet
+// symbols - unique
+
+//object
+
+console.log(typeof "name")
+
+console.log(typeof rank)
+
+console.log(typeof age)
+
+console.log(typeof state)
+
+console.log(typeof null)
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+// use let instead of var cuz of block scoping
+// Js is case sensitive
+
+const idName = "GoatedV1"
+let clan = "5"
+var rank = "Six Paths"
+chars = "216"
+let Clan = "10"
+const IdName = "V2"
+Clan = "37"
+
+
+console.table([idName,clan,rank,chars,IdName,Clan])
