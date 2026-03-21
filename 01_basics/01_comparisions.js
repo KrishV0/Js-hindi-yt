82b806ca7fbc2392dc8aac29ec309a2ad2f1dc10
KrishV0 <krishsharma2606@gmail.com>
2026-03-21 09:55:22 +0000

    comparisions

---
+++ 01_basics/01_comparisions.js
@@ -0,0 +1,16 @@
+console.log("2" ==2)
+console.log("00002" ==2)
+
+console.log(null>0)  
+console.log(null==0)
+console.log(null<=0)
+
+
+
+/*== and >,<,>=,<= works differently
+the latter converts null to 0 whereras == doesnt
+*/
+
+console.log(undefined>=0)
+console.log(undefined<=0)
+console.log(undefined==0)
