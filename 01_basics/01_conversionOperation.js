2ac3496da98248105a5627a6e15dc27a39396592
KrishV0 <krishsharma2606@gmail.com>
2026-03-21 08:54:24 +0000

    conversionOp

---
+++ 01_basics/01_conversionOperation.js
@@ -0,0 +1,26 @@
+let num = undefined
+let isnum = Number(num)
+console.log(isnum)
+console.log(typeof isnum)
+
+// string,storing nothing,undefined - NaN(actually a number)
+// true or false - 1 or 0 respectively
+
+let Accname = null
+let isAccname = String(Accname)
+console.log(isAccname)
+console.log(typeof isAccname)
+
+//storing nothing - Undefined
+//number,null,Nan,true,false - string
+
+let isAccount
+let isAccnameBoolean = Boolean(isAccount)
+console.log(isAccnameBoolean)
+console.log(typeof isAccnameBoolean)
+
+//0-false,1-true
+//String-true
+//Random no-true
+//undefined - false
+//store nothing - false
